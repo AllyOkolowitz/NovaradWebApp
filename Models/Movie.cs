1801656e260519cc2895a33e66eba49596f09a5f
Alexandria Okolowitz Vu <Alexandria@go.byuh.edu>
2019-09-12 13:05:18 -0600

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Display(Name = "Account Number")]
+        public string Title { get; set; }
+        [Display(Name = "Account Name")]
+        public string Genre { get; set; }
+
+        [Display(Name = "Invoice Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Pages/Create.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using System;
+using System.Threading.Tasks;
+
+
+namespace WebApp.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApp.Models.WebAppContext _context;
+
+        public CreateModel(WebApp.Models.WebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
